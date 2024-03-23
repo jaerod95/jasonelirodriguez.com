3e1c656e4bdd9ca40b48e9713c5f09cee0e385d2
jason@dittowords.com <jason@dittowords.com>
2024-03-22 23:27:21 -0700

    basic layouts

---
+++ src/app/blog/(posts)/layout.tsx
@@ -0,0 +1,20 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Blog",
+  description: "Blog post on various topics.",
+};
+
+export default function Layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="w-full min-h-svh py-8">
+      <article className="prose prose-slate dark:prose-invert md:container md:mx-auto px-6 md:px-24 lg:36 py-4l">
+        {children}
+      </article>
+    </div>
+  );
+}
---
+++ src/app/blog/page.tsx
@@ -0,0 +1,18 @@
+export default function BlogPosts() {
+  return (
+    <>
+      <h1>Blog Posts</h1>
+      <ul>
+        <li>
+          <a href="/blog/1">Post 1</a>
+        </li>
+        <li>
+          <a href="/blog/2">Post 2</a>
+        </li>
+        <li>
+          <a href="/blog/3">Post 3</a>
+        </li>
+      </ul>
+    </>
+  );
+}
